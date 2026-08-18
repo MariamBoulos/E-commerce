415bc842157adfe988c8f6860f5a780520a14b0a
Mariam Boulos <mariamboulos80@gmail.com>
2026-08-18 04:26:09 +0300

    Updated

---
+++ shop-service/shop-service/src/main/java/com/shop_service/Order_Product.java
@@ -0,0 +1,91 @@
+package com.shop_service;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity(name="order_product")
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"order_id", "product_id"}))
+public class Order_Product {
+	
+	public Order_Product() {
+		
+	}
+	
+	@Id
+	@GeneratedValue
+	private Integer orderProductId;
+	
+	private Integer productId;
+	
+	private Integer quantity;
+	
+	private BigDecimal price;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id", nullable = false)
+	private Order order;
+
+	public Order_Product(Integer orderProductId, Integer productId, Integer quantity, BigDecimal price, Order order) {
+		super();
+		this.orderProductId = orderProductId;
+		this.productId = productId;
+		this.quantity = quantity;
+		this.price = price;
+		this.order = order;
+	}
+
+	public Integer getOrderProductId() {
+		return orderProductId;
+	}
+
+	public void setOrderProductId(Integer orderProductId) {
+		this.orderProductId = orderProductId;
+	}
+	
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public String toString() {
+		return "Order_Product [orderProductId=" + orderProductId + ", productId=" + productId + ", quantity=" + quantity
+				+ ", price=" + price + ", order=" + order + "]";
+	}
+
+}
